1c34e5e2badb84739ab8c926201a5d0aa80d002e
Amily Wu <amilywu2@amd.com>
2026-01-22 21:35:58 +0000

    Add py

--- amdsharktuner/dispatch_tuner/compare_pipeline.py
+++ amdsharktuner/dispatch_tuner/compare_pipeline.py
@@ -28,12 +28,16 @@ for csv_dir_list in csv_dir_name_list:
     tf_win = 0
     vd_win = 0
     tie = 0
+    diff = []
     for file in sorted(vd_dir.glob("*.csv")):
         name = file.name
         vd_df = pd.read_csv(file)
         tf_df = pd.read_csv((Path(tf_dir) / name))
         vd_opt = vd_df["benchmark_speedup"].min()
         tf_opt = tf_df["benchmark_speedup"].min()
+        if pd.isna(vd_opt) or pd.isna(tf_opt):
+            continue
+        diff.append(vd_opt-tf_opt)
         if abs(vd_opt-tf_opt) > max([vd_opt,tf_opt])*win_thre:
             if tf_opt < vd_opt:
                 tf_win += 1
@@ -41,8 +45,16 @@ for csv_dir_list in csv_dir_name_list:
                 vd_win += 1
         else:
             tie += 1
-
     print(f"{csv_dir_name.split('_')[0]}")
+    
+    
+    diff_avg = sum(diff) / len(diff) if diff else None
+    if diff_avg < 0:
+        print(f"VD faster: {abs(diff_avg)*100:.2f}% of baseline benchmark time in avg")
+    else:
+        print(f"TF faster: {abs(diff_avg)*100:.2f}% of baseline benchmark time in avg")
+
+    
     tie = len(sorted(vd_dir.glob("*.csv"))) - (tf_win+vd_win)
     print(f"WIN: opt_diff > slower_opt * {win_thre}")
     print(f"Total csv: {len(sorted(vd_dir.glob('*.csv')))}, DIFF:{tf_win+vd_win}, TIE:{tie}")
